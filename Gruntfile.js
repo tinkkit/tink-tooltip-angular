dc09fec2c6daee234efad1c9dbf6b18ea21d6409
vincent bouillart <vincent.bouillart@appfoundry.be>
2015-06-26 15:45:47 +0200

    init commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,7 +22,7 @@ module.exports = function (grunt) {
     dist: 'dist',
     version: currentversion,
     module: require('./package.json').name,
-    domain: 'tink.skeleton'
+    domain: 'tink.tooltip'
   };
 
   // Define the configuration for all the tasks
